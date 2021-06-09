f960ca041da9dce30c32699d11b1dc6c6e580397
Daniel Bast <2790401+dbast@users.noreply.github.com>
2021-06-09 14:38:07 +0200

    Add extra args support

--- build.py
+++ build.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-import glob
+import argparse
 import os
 import subprocess
 import sys
-from argparse import ArgumentParser
+from pathlib import Path
 from shutil import which
 
 
@@ -13,28 +13,27 @@ def setup_environment(ns):
     os.environ["UPLOAD_PACKAGES"] = "False"
 
 
-def run_build(ns):
+def run_build(ns, extra_args):
+    dot_abs_folder = (Path(__file__).parent.absolute() / ".abs").as_posix()
     cmd = [
         "conda",
         "build",
         "./recipe",
         "-m",
-        f"./.abs/{ns.config}.yaml",
+        f"{dot_abs_folder}/{ns.config}.yaml",
         "--suppress-variables",
         "--clobber-file",
-        "./.abs/clobber_noarch.yaml",
-    ]
+        f"{dot_abs_folder}/clobber_noarch.yaml",
+    ] + extra_args
     if ns.output_folder:
-        cmd += [
-            "--output-folder",
-            ns.output_folder
-        ]
+        cmd += ["--output-folder", ns.output_folder]
     print(f"Calling: {cmd}")
     subprocess.check_call(cmd)
 
 
 def verify_config(ns):
-    valid_configs = {os.path.basename(f)[:-5] for f in glob.glob(".abs/*.yaml")}
+    dot_abs_folder = Path(__file__).parent.absolute() / ".abs"
+    valid_configs = {os.path.basename(f)[:-5] for f in dot_abs_folder.glob("*.yaml")}
     print(f"valid configs are {valid_configs}")
     if ns.config in valid_configs:
         print("Using " + ns.config + " configuration")
@@ -42,11 +41,11 @@ def verify_config(ns):
 
 
 def main(args=None):
-    p = ArgumentParser("build")
+    p = argparse.ArgumentParser("build")
     p.add_argument("config", default="conda_build_config", nargs="?")
     p.add_argument("--output-folder", action="store")
 
-    ns = p.parse_args(args=args)
+    ns, extra_args = p.parse_known_args(args=args)
 
     verify_config(ns)
 
@@ -59,7 +58,7 @@ def main(args=None):
         subprocess.check_call(["conda", "install", "conda-build", "-y"])
 
     # build
-    run_build(ns)
+    run_build(ns, extra_args)
 
 
 if __name__ == "__main__":
